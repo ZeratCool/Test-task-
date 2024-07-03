9fbaab83566d5fcd7520b324e0931eed17bd7975
ZeratCool <kuevda002@gmail.com>
2024-07-03 18:43:12 +0300

    Create auth model

--- lunch_service/accounts/models.py
+++ lunch_service/accounts/models.py
@@ -1,4 +1,15 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class CasualUser(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return f"{self.username}"
--- lunch_service/lunch_service/urls.py
+++ lunch_service/lunch_service/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from lunch_service import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('api/auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
     path('accounts/', include('accounts.urls')),
